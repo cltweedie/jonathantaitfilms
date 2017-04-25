dd5db60fd00fba9a0717064b2e6aba60a0208f57
Chris Tweedie <cltweedie@gmail.com>
2017-04-25 22:12:20 +0100

    Carrierwave and AWS integration

--- app/controllers/admin/albums_controller.rb
+++ app/controllers/admin/albums_controller.rb
@@ -1,8 +1,16 @@
 class Admin::AlbumsController < ApplicationController
-  
   layout 'admin'
 
   def index
+  end
+  
+  def create
+    Album.create!(album_params)
+    redirect_to admin_albums_path
+  end
+  
+  def album_params
+    params.require(:album).permit(:title, :description)
   end
   
 end
---
+++ app/controllers/admin/photos_controller.rb
@@ -0,0 +1,30 @@
+class Admin::PhotosController < ApplicationController
+  layout 'admin'
+  
+  before_action :get_album
+  
+  def index
+  end
+  
+  def create
+    @album.photos << Photo.new(photo_params)
+    @album.save!
+    redirect_to admin_albums_path
+  end
+  
+  def destroy
+    Photo.find(params[:id]).destroy
+    redirect_to admin_album_photos_path(@album)
+  end
+  
+  private
+  
+  def photo_params
+    params.require(:photo).permit(:title, :caption, :image)
+  end
+  
+  def get_album
+    @album = Album.find(params[:album_id])
+  end
+  
+end
